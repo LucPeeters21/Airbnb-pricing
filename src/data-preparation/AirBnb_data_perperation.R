1a5b3ce2a11cbb0469cb87402ac4209d2de4fea5
Mike Verweij <m.r.j.a.verweij@tilburguniversity.edu>
2022-03-02 23:49:50 +0100

    convert price column to numeric

--- src/data-preparation/AirBnb_data_perperation.R
+++ src/data-preparation/AirBnb_data_perperation.R
@@ -17,6 +17,10 @@ host_id_count <- count(Airbnb_listings, host_id)
 
 #Histogram
 
+#Remove dollar signs from price and convert to numeric
+Airbnb_listings[]<-lapply(Airbnb_listings, gsub, pattern="$", fixed=TRUE, replacement="")
+Airbnb_listings$price<-lapply(Airbnb_listings$price, gsub, pattern=",", fixed=TRUE, replacement="")
+Airbnb_listings$price<-as.numeric(Airbnb_listings$price)
 
 # Scatterplot
 ggplot(Airbnb_listings, aes(x=number_of_reviews, y=price))+ geom_point()
@@ -26,6 +30,9 @@ ggplot(Airbnb_listings, aes(x=neighbourhood, y=price))+ geom_point()
 mean_prices_neighbourhoods <- Airbnb_listings %>% select(neighbourhood, price) %>%  group_by(neighbourhood) %>%  summarize_all(mean, na.rm=TRUE)
 View(mean_prices_neighbourhoods)
 barplot(table(mean_prices_neighbourhoods$neighbourhood))
+
+mean_prices_country <- Airbnb_listings %>% select(link, price) %>%  group_by(link) %>%  summarize_all(mean, na.rm=TRUE)
+View(mean_prices_country)
 
 #simple regression
 regr_neigh_price <- lm(price ~ number_of_reviews+room_type+neighbourhood+, Airbnb_listings)
